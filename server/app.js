6d04adf1a6b97d2669ba40ade9020a8c854e7cb7
Tarun Juyal <tarunJuyalHere@gmail.com>
2021-01-06 12:34:33 +0530

    commit

--- server/app.js
+++ server/app.js
@@ -36,9 +36,9 @@ app.listen(port, () => {
   console.log(`Server Started at ${port}...`);
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("./build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname,"build","index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"build","index.html"));
+  });
+}
